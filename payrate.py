74754d161221457582bd7272516f360e301d1464
Jason Honeyager <jasonhoneyager@gmail.com>
2016-05-12 19:00:40 -0500

    Updated Payroll Generator to Mark V

--- payrate.py
+++ payrate.py
@@ -1,27 +1,36 @@
-print ("Payroll Generator Mark III")
+print ("Payroll Generator Mark V")
 def HourlyWage():
-    payrate = input("Enter your hourly rate:")
-    try:
-        payrate = float(payrate)
-    except:
-        print("Please enter a number:")
-        payrate = -1
+    complete = False
+    while not complete:
+        payrate = input("Enter your hourly rate:")
+        try:
+            payrate = float(payrate)
+            if payrate == 0:
+                print("I highly doubt you work for free...")
+                continue
+            complete = True
+        except:
+            print("Please enter a number:")
     return payrate
-def HoursWorked():				
-    hours = input("How many hours did you work?")
-    try:
-        hours = float(hours)
-    except:
-        print("Please enter a number:")
-        hours = -1
+def HoursWorked():
+    complete = False
+    while not complete:
+        hours = input("How many hours did you work?")
+        try:
+            hours = float(hours)
+            complete = True
+        except:
+            print("Please enter a number:")
     return hours
 def PaidTimeOff():
-    vacation = input("How many Vacation/PDO hours did you use?")
-    try:
-        vacation = float(vacation)
-    except:
-        print("Please enter a number:")
-        vacation = -1
+    complete = False
+    while not complete:
+        vacation = input("How many Vacation/PDO hours did you use?")
+        try:
+            vacation = float(vacation)
+            complete = True
+        except:
+            print("Please enter a number:")
     return vacation
 def Overtime(hours):
     if hours - 40 <= 0:
@@ -32,7 +41,7 @@ def Overtime(hours):
 def Paycheck():
     wages = (hours + vacation) * payrate
     salary = overtime + wages
-    print("Your gross pay is ${0} at the payrate of {1}".format(salary,payrate),"/hr.")
+    print("Your gross pay is ${0} at the payrate of ${1}".format(salary,payrate),"/hr.")
 payrate = HourlyWage()
 hours = HoursWorked()
 vacation = PaidTimeOff()
